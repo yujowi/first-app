c6e6f8451eff8dfffa4606a7cb4c97ad5f4cb787
Yuri <yuri@Yuris-MacBook-Pro.local>
2021-11-08 11:51:59 +0100

    updated port

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 const app = express();
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("buuhjah!");
 });
 
